dbe25fedbd07f334a8e31d31415a4fc27371f0e5
roushan-sheik <roushansheik@gmail.com>
2025-07-30 10:30:08 +0600

    [style]: Header styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { CheckSquare } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="bg-[color:var(--color-brand-500)] shadow-lg">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo and Brand */}
-          <div className="flex items-center space-x-3">
-            <div className="bg-white p-2 rounded-lg shadow-sm">
-              <CheckSquare className="h-6 w-6 text-[color:var(--color-brand-500)]" />
+          <Link href={"/"}>
+            <div className="flex items-center cursor-pointer space-x-3">
+              <div className="bg-white p-2 rounded-lg shadow-sm">
+                <CheckSquare className="h-6 w-6 text-[color:var(--color-brand-500)]" />
+              </div>
+              <h1 className="text-heading2 font-bold text-white">Tasky</h1>
             </div>
-            <h1 className="text-heading2 font-bold text-white">Tasky</h1>
-          </div>
+          </Link>
 
           {/* Navigation - Hidden on mobile, shown on desktop */}
           <nav className="hidden md:flex items-center space-x-8">
@@ -38,12 +41,16 @@ const Header = () => {
 
           {/* Auth Buttons */}
           <div className="flex items-center space-x-4">
-            <button className="text-body2 text-white hover:text-[color:var(--color-brand-100)] transition-colors duration-200 hidden sm:block">
-              LOGIN
-            </button>
-            <button className="bg-white text-[color:var(--color-brand-500)] px-4 py-2 rounded-md text-body2 font-medium hover:bg-[color:var(--color-neutral-50)] transition-colors duration-200 shadow-sm">
-              REGISTER
-            </button>
+            <Link href={"/login"}>
+              <button className="text-body2 cursor-pointer text-white hover:text-[color:var(--color-brand-100)] transition-colors duration-200 hidden sm:block">
+                Login
+              </button>
+            </Link>
+            <Link href={"/signup"}>
+              <button className="bg-white  cursor-pointer text-[color:var(--color-brand-500)] px-4 py-2 rounded-md text-body2 font-medium hover:bg-[color:var(--color-neutral-50)] transition-colors duration-200 shadow-sm">
+                SignUp
+              </button>
+            </Link>
           </div>
         </div>
       </div>
